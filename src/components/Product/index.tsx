445eaabd91abd69f0f85c26aa362967bb49883cc
Thiago <thimarsilva18@gmail.com>
2022-12-08 20:02:47 -0300

    Add to cart

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { Text } from '../Text';
+// import PlusCircle from '../../assets/plus-circle';
+
+import { IoIosAddCircleOutline, IoIosRemoveCircleOutline } from 'react-icons/io';
+
 import { ProductContainer } from './styles';
 
 function Product({product}) {
+  const [count, setCount] = useState(0
+    // count: 0,
+  );
+
+  // const volumeDecrease = () => {
+  //   if(count > 0 && count < 100){
+  //     setControls({
+  //       count: controls.count -1
+  //     });
+  //   }
+  // };
+
   return (
     <ProductContainer>
       <div className='image'/>
@@ -10,6 +27,22 @@ function Product({product}) {
       <div className='text-content'>
         <Text>Espetinho</Text>
         <Text><small>R$ 10,00</small></Text>
+      </div>
+
+      <div className='add'>
+        <Text className='addTo'>Adicionar ao carrinho</Text>
+
+        <div className='funcAdd'>
+          <button onClick={() => setCount(count - 1)}>
+            <IoIosRemoveCircleOutline />
+          </button>
+
+          <Text>{count}</Text>
+
+          <button onClick={() => setCount(count + 1)}>
+            <IoIosAddCircleOutline />
+          </button>
+        </div>
       </div>
     </ProductContainer>
   );
--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -6,8 +6,6 @@ export const ProductContainer = styled.div`
     border-radius: 15px;
     border: 1px solid black;
     overflow: hidden;
-    /* margin-left: 8px;
-    margin-right: 8px; */
 
     display: flex;
     flex-direction: column;
@@ -17,13 +15,39 @@ export const ProductContainer = styled.div`
         width: 100%;
         height: 120px;
         background-color: black;
-    }
+    };
 
     .text-content {
         padding: 8px;
         display: flex;
         flex-direction: column;
         gap: 5px;
-    }
+    };
 
+    .add {
+        display: flex;
+        flex-direction: column;
+        padding: 8px;
+        width: 100%;
+
+        .addTo {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        };
+
+        .funcAdd {
+            display: flex;
+
+            align-items: center;
+            justify-content: space-around;
+        };
+
+        button {
+            width: 40px;
+            height: 40px;
+            border: none;
+            border-radius: 100%;
+        };
+    };
 `;
